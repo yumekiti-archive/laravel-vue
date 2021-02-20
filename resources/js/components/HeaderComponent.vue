f0eae735dc67e35c9e3e8f60793210a4eb2b66ce
yumekiti <yumekiti120440@gmail.com>
2021-02-20 23:49:25 +0000

    add: vue

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul class="nav nav-tabs">
+            <li class="nav-item" v-for="(link,index) in links" :key="index">
+                <router-link :to="link.path">
+                    <span :class="{active:index === isActive}" class="nav-link" @click="change(index)">{{ link.path }}</span>
+                </router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"head",
+        data() {
+            return{
+                links:[
+                    {path:"home"},
+                    {path:"todo"}
+                ],
+
+                isActive: 0,
+            }
+        },
+        methods:{
+            change: function(num){
+                this.isActive = num
+            }
+        }
+    }
+</script>
---
+++ resources/js/components/TodoComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="todo">
+    <div class="text-center">
+      <div id="todo-list-example">
+        <form v-on:submit.prevent="addNewTodo">
+          <label for="new-todo">Add a todo</label>
+          <input
+            v-model="newTodoText"
+            id="new-todo"
+            placeholder="E.g. Feed the cat"
+          />
+          <button>Add</button>
+        </form>
+        <ul>
+          <todo-item-2
+            v-for="(todo, index) in todos"
+            :key="todo.id"
+            :title="todo.title"
+            @remove="todos.splice(index, 1)"
+          ></todo-item-2>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data() {
+    return {
+      newTodoText: '',
+      todos: [
+        {
+          id: 1,
+          title: 'Do the dishes'
+        },
+        {
+          id: 2,
+          title: 'Take out the trash'
+        },
+        {
+          id: 3,
+          title: 'Mow the lawn'
+        }
+      ],
+      nextTodoId: 4
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    }
+  }
+}
+</script>
+
+<style>
+  li {
+    list-style: none;
+  }
+</style>
